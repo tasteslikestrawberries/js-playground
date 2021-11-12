85f818accfd5825fcbb6f8d3c21abb79fab4a008
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-11-12 13:45:09 +0100

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,25 @@
+const posts = [
+  { title: "Post 1", body: "This is post one" },
+  { title: "Post 2", body: "This is post two" },
+];
+
+//mimicking fetching from a server
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML += output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+//calling function
+createPost({ title: "Post 3", body: "This is post three" }, getPosts);
