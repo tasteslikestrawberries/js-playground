8f3a4f8b7df5f7ff0ca2399984b1a9775d531f3f
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-02-07 13:59:39 +0100

    'incrementing'

---
+++ incrementing.js
@@ -0,0 +1,5 @@
+let count = 0;
+//count++//postfix returns the value first and then increments
+console.log(count) // the count value is now 1
+
+console.log(++count) //increments and then returns the (now incremented) value
